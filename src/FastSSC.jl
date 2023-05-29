a477f26528de4e62b9aaf038da048ad4b04f8eaf
Marco Bonici <58727599+marcobonici@users.noreply.github.com>
2023-05-29 17:28:40 +0200

    REmoved tturbo macro

--- src/FastSSC.jl
+++ src/FastSSC.jl
@@ -5,7 +5,7 @@ using LoopVectorization
 function SSC_integral(volume_element, WA, WB, WC, WD, responseAB, responseCD, σ², nz, nl, ntomo, dz)
     result = zeros(length(ℓ_array), length(ℓ_array), ntomo, ntomo, ntomo, ntomo)
 
-    @tturbo for idxli in 1:nl
+    @turbo for idxli in 1:nl
         for idxlj in 1:nl
             for i in 1:ntomo
                 for j in 1:ntomo
